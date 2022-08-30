19a822cf9a2e1225407a6e65f95183286d460ce7
Nattapong <nattapong.pim@ku.th>
2022-08-30 21:47:21 +0700

    first commit

---
+++ src/main/java/th/ac/ku/lab9restaurant/Lab9RestaurantApplication.java
@@ -0,0 +1,13 @@
+package th.ac.ku.lab9restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab9RestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab9RestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/th/ac/ku/lab9restaurant/Lab9RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.ku.lab9restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab9RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
